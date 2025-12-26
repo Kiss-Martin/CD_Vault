1c584981a5bd573c9db011c5628b80f8badb52a0
Kiss-Martin <90604013+Kiss-Martin@users.noreply.github.com>
2025-12-26 17:38:41 +0100

    dock.net

---
+++ CD_Vault/CD_Vault/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CD_Vault
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CD_Vault/CD_Vault/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CD_Vault
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
